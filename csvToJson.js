0d16e7542ca809be8700fb68c928e74da4fa636a
riandyhasan <riandyhsn@gmail.com>
2023-11-25 17:36:22 +0700

    update data

--- csvToJson.js
+++ csvToJson.js
@@ -2,25 +2,74 @@ const fs = require("fs");
 const csv = require("csv-parser");
 
 // Replace 'input.csv' with your CSV file name and 'output.json' with the desired JSON output file name
-const csvFilePath = "lengkap_plus_prov.csv";
-const jsonFilePath = "sumut.json";
-
-const results = [];
+const csvFilePath = "lengkap.csv";
+let provinces = [
+  {
+    name: "SUMATERA UTARA",
+    path: "sumut.json",
+  },
+  {
+    name: "RIAU",
+    path: "riau.json",
+  },
+  {
+    name: "KEPULAUAN RIAU",
+    path: "kep_riau.json",
+  },
+  {
+    name: "BANGKA BELITUNG",
+    path: "bangka_belitung.json",
+  },
+  {
+    name: "KALIMANTAN BARAT",
+    path: "kalbar.json",
+  },
+  {
+    name: "KALIMANTAN TIMUR",
+    path: "kaltim.json",
+  },
+  {
+    name: "KALIMANTAN UTARA",
+    path: "kaltara.json",
+  },
+  {
+    name: "PAPUA",
+    path: "papua.json",
+  },
+  {
+    name: "PAPUA BARAT",
+    path: "papua_barat.json",
+  },
+];
 
 fs.createReadStream(csvFilePath)
   .pipe(csv())
   .on("data", (data) => {
-    if (data["PROVINSI"] == "SUMATERA UTARA") {
-      results.push(data);
-    }
+    provinces.forEach((province) => {
+      if (data["PROVINSI"] === province.name) {
+        if (!province.results) {
+          province.results = [];
+        }
+        province.results.push(data);
+      }
+    });
   })
   .on("end", () => {
-    // Write the parsed data to a JSON file
-    fs.writeFile(jsonFilePath, JSON.stringify(results, null, 2), (err) => {
-      if (err) {
-        console.error("Error writing JSON file:", err);
-      } else {
-        console.log("JSON file written successfully.");
+    provinces.forEach((province) => {
+      if (province.results) {
+        fs.writeFile(
+          province.path,
+          JSON.stringify(province.results, null, 2),
+          (err) => {
+            if (err) {
+              console.error(
+                `Error writing JSON file for ${province.name}:`,
+                err
+              );
+            }
+          }
+        );
+        console.log(`JSON file for ${province.name} written successfully.`);
       }
     });
   });
